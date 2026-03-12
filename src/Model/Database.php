321edc351c4fc97bc91989fc2b0e7d03cc6533ec
Theo <theo.prieur1@viacesi.fr>
2026-03-12 11:59:43 +0100

    First DataBase request for the home page

--- src/Model/Database.php
+++ src/Model/Database.php
@@ -1,1 +1,19 @@
 <?php
+
+class Database {
+    private static PDO $pdo;
+
+    public static function getConnection(): PDO {
+        if (!isset(self::$pdo)) {
+            self::$pdo = new PDO(
+                "mysql:host=localhost;dbname=NOM_NOTRE_BDD;charset=utf8",               // IL FAUT QU'ON CHANGE LE NOM AVEC NOTRE VRAI NOM APRES     OU QU'ON VOIT POUR UNE AUTRE METHODE
+                "root",                                                                 // IL FAUT QU'ON REMPLISSE AVEC LE BON NOM D'USER
+                ""                                                                      // IL FAUT QU'ON REMPLISSE AVEC LE BON MDP
+            );
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$pdo;
+    }
+}
+
+?>
---
+++ src/Model/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'Database.php';
+
+class HomeModel {
+    private PDO $pdo;
+
+    public function __construct() {
+        $this->pdo = Database::getConnection(); 
+    }
+
+
+public function getOffres() {     // RéRecupere toutes les offres actives avec le nom de l'entreprise. Pour les cartes a droite de l'ecran.
+    $stmt = $this->pdo->prepare("
+        SELECT o.Id_offre, o.Titre, o.Description, o.Remuneration, e.Nom AS Entreprise_Nom
+        FROM Offre o
+        JOIN Entreprise e ON o.Id_entreprise = e.Id_entreprise
+        WHERE e.Est_actif = TRUE
+        ORDER BY o.Date_offre DESC
+    ");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+
+}  ?>
