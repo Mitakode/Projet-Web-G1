2e5dddf0026e6cc276e06af116957e8ab37c1240
Adri1 <adrien.melle@laposte.net>
2026-03-24 14:10:47 +0100

    rebase pr

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,18 +4,10 @@ use App\Controller\AuthController;
 use App\Controller\HomeController;
 use App\Controller\AccountController;
 use App\Core\View;
-use App\Controller\FicheController;
 
 $uri    = $_GET['uri'] ?? '/';
 $method = $_SERVER['REQUEST_METHOD'];
 
-$authController = new AuthController();
-$homeController = new HomeController();
-$ficheController = new FicheController();
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $authController    = new AuthController();
 $homeController    = new HomeController();
 $accountController = new AccountController();
