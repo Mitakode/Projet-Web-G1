81b257eb655335b8fd0fec788376acba1524a102
Gaarayte <gilbert.david.clement@gmail.com>
2026-03-27 11:57:44 +0100

    Implémentation du CSS effectué, legere correction dans Router (+ centralisaition du CSS)

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -72,6 +72,7 @@ switch ($uri) {
             '/pilote'          => $dashboardController->Pilote(),
             '/add_pilote'      => $dashboardController->addPilote(),
         };
+        break;
     case '/candidater':
         if ($method === 'POST') {
             $candidatureController->submit();
